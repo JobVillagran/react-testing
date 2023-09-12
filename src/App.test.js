7a9c042354249a19e356e21679b581a23c865af5
JobVillagran <jobvillagran@gmail.com>
2023-09-12 08:26:06 -0600

    mod(New test: App.test.js)

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,13 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByTestId('learn-link');
   expect(linkElement).toBeInTheDocument();
+  expect(linkElement.getAttribute('href')).toContain('ultimateqa.com');
+});
+
+test('url is correct', () => {
+  render(<App />);
+  const linkElement = screen.getByTestId('learn-link');
+  expect(linkElement.href).toContain('ultimateqa.com');
 });
